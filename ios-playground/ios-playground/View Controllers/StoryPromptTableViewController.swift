9bef765b49438ef3302724e69a47cb83d8bb845d
Caique Minhare <caique.minhare@xpi.com.br>
2022-03-17 09:18:33 -0300

    📦 Funcionalidade: Adicionando botão para navegar para o controller de adicionar prompt

--- ios-playground/ios-playground/View Controllers/StoryPromptTableViewController.swift
+++ ios-playground/ios-playground/View Controllers/StoryPromptTableViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 class StoryPromptTableViewController: UITableViewController {
     var storyPromptEntries: [StoryPromptEntry] = [StoryPromptEntry]()
     
+    
+    
+    @IBAction func onAddStoryPromptClicked(_ sender: Any) {
+        performSegue(withIdentifier: "ShowAddStoryPrompt", sender: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
